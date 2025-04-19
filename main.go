6880a00b3270cc4290c36b9420e167e5922c3b7f
ciaranshan <shanac030@gmail.com>
2025-04-19 12:31:32 +0100

    refactor to separate fn

--- main.go
+++ main.go
@@ -14,24 +14,34 @@ func main() {
 
 	flag.Parse()
 
-	if *urlArg == ""{
+	if *urlArg == "" {
 		fmt.Printf("url must be set using -u\n")
 		os.Exit(1)
 	}
 
-	url, err := url.Parse(*urlArg)
+	testURL, err := url.Parse(*urlArg)
 	if err != nil {
 		fmt.Printf("parsing url: %s", err.Error())
 		os.Exit(1)
 	}
 
-	for range *n {
-		resp, err := http.Get(url.String())
+	err = loadTest(testURL, *n)
+	if err != nil {
+		fmt.Printf("running load test: %s", err.Error())
+		os.Exit(1)
+	}
+
+}
+
+func loadTest(testURL *url.URL, n int) error {
+	for range n {
+		resp, err := http.Get(testURL.String())
 		if err != nil {
-			fmt.Printf("making http request: %s\n", err.Error())
-			os.Exit(1)
+			return fmt.Errorf("making http request: %w", err)
 		}
 
 		fmt.Printf("response code: %d\n", resp.StatusCode)
 	}
+
+	return nil
 }
