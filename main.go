67c1c7a2764031f7d97b1db8c8f3b71e0165c66a
ciaranshan <shanac030@gmail.com>
2025-04-19 12:28:02 +0100

    number of requests flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -8,23 +9,29 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) == 0 {
-		fmt.Printf("argument must be provided\n")
+	urlArg := flag.String("u", "", "URL to load test against")
+	n := flag.Int("n", 10, "Number of requests to make")
+
+	flag.Parse()
+
+	if *urlArg == ""{
+		fmt.Printf("url must be set using -u\n")
 		os.Exit(1)
 	}
 
-	url, err := url.Parse(args[0])
+	url, err := url.Parse(*urlArg)
 	if err != nil {
 		fmt.Printf("parsing url: %s", err.Error())
 		os.Exit(1)
 	}
 
-	resp, err := http.Get(url.String())
-	if err != nil {
-		fmt.Printf("making http request: %s", err.Error())
-		os.Exit(1)
-	}
+	for range *n {
+		resp, err := http.Get(url.String())
+		if err != nil {
+			fmt.Printf("making http request: %s\n", err.Error())
+			os.Exit(1)
+		}
 
-	fmt.Printf("response code: %d", resp.StatusCode)
+		fmt.Printf("response code: %d\n", resp.StatusCode)
+	}
 }
